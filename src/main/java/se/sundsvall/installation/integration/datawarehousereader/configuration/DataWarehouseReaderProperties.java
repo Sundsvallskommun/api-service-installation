7b8efe7f4533127256837c4d8e380bef33ebe16d
AndreSoderlund <andre.soderlund@b3.se>
2024-04-03 11:59:58 +0200

    config-fix

--- src/main/java/se/sundsvall/installation/integration/datawarehousereader/configuration/DataWarehouseReaderProperties.java
+++ src/main/java/se/sundsvall/installation/integration/datawarehousereader/configuration/DataWarehouseReaderProperties.java
@@ -3,7 +3,7 @@ package se.sundsvall.installation.integration.datawarehousereader.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
-@ConfigurationProperties("integration.datawarehousereader")
-public record DataWarehouseReaderProperties(@DefaultValue("5") int connectTimeout,
-                                            @DefaultValue("20") int readTimeout) {
+@ConfigurationProperties("config.integration.datawarehousereader")
+public record DataWarehouseReaderProperties(@DefaultValue("20") int connectTimeout,
+                                            @DefaultValue("115") int readTimeout) {
 }
